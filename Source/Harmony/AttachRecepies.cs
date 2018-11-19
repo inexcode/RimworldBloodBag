036ac2c612eb772a251e7dbcecabf711be034f54
XeoNovaDan <toghill7252010@hotmail.co.uk>
2018-11-19 22:43:56 +0000

    Android tiers fix
    Also applies to any other race mods that add races with null recipes

--- Source/Harmony/AttachRecepies.cs
+++ Source/Harmony/AttachRecepies.cs
@@ -19,6 +19,8 @@ namespace BloodTypes.Harmony
             foreach (var humanoidRace in humanoidRaces)
             {
                 Log.Message("Adding in draw blood");
+                if (humanoidRace.recipes == null)
+                    humanoidRace.recipes = new List<RecipeDef>();
                 humanoidRace.recipes.Add(DonateBlood);
             }
         }
